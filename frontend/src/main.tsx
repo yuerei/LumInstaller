99056516aefb92f366f89e97ea673419afeedfa6
eve <63238154+yuerei@users.noreply.github.com>
2026-06-29 14:28:14 +0700

    feat: add shadcn ui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,9 @@
-import React from 'react'
+import { StrictMode } from "react";
 import {createRoot} from 'react-dom/client'
-import './style.css'
-import App from './App'
-
-const container = document.getElementById('root')
-
-const root = createRoot(container!)
-
-root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-)
+import "./index.css";
+import App from "./App.tsx";
+import { Toaster } from "@/components/ui/sonner";
+createRoot(document.getElementById("root")!).render(<StrictMode>
+      <App />
+      <Toaster position="bottom-center" duration={1000}/>
+  </StrictMode>);
