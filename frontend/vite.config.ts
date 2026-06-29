afa9242f0c7f36002d309afef845b10845794655
eve <63238154+yuerei@users.noreply.github.com>
2026-06-29 13:53:24 +0700

    chore: update TypeScript configuration and eslint setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import path from "path";
+import fs from "fs";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+const wailsJsonPath = path.resolve(__dirname, "../wails.json");
+const wailsJson = JSON.parse(fs.readFileSync(wailsJsonPath, "utf-8"));
+const appVersion = wailsJson.info.productVersion;
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), tailwindcss()],
+  resolve: {
+      alias: {
+          "@": path.resolve(__dirname, "./src"),
+      },
+  },
+  define: {
+      __APP_VERSION__: JSON.stringify(appVersion),
+  },
 })
